c9ccdf7d9522f05bc127aea15ca3a8b79ee7c6a2
fengxian.guo <guofengxian@veredholdings.com>
2020-06-14 19:34:35 +0800

    add

--- miniprogram/pages/problem/problem.js
+++ miniprogram/pages/problem/problem.js
@@ -17,6 +17,9 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+
+    console.log("====>", options);
+
     const { title, content } = options;
 
     let result = app.towxml(title, 'markdown');
